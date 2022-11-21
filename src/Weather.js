92afaacf16e5661d024a9af2dd69827c7fa24b28
evatok <teo@ukr.net>
2022-11-21 18:22:42 +0100

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -10,13 +10,14 @@ export default function Weather(){
      type="search" 
      placeholder="Enter a city..."
      className="form-control"
+     autoFocus="on"
      />
      </div>
      <div className="col-3">
      <input 
      type="submit" 
      value="Search"
-     className="btn btn-primary"
+     className="btn btn-primary w-100"
      />
      </div>
      </div>
@@ -30,10 +31,14 @@ export default function Weather(){
     </ul>
     <div className="row">
         <div className="col-6">
+         <div className="mt-3">
             <img 
             src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="rainy"/>
-            8°C
+            alt="rainy"
+            />
+            
+            <span className="temperature" >8</span>
+            <span className="units">°C</span></div>
         </div>
         <div className="col-6">
           <ul>
