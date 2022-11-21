789d022182f8e8f5cf0936672e8809abc562357d
evatok <teo@ukr.net>
2022-11-21 15:19:02 +0100

    added github url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-Weather App
+<h1>Weather App</h1>
+<footer>
+  This project was coded by {" "}
+  <a href="https://bucolic-liger-7233b0.netlify.app/index.html" target="_blank">Eva Tokarchuk</a>
+   {" "}and is{" "} 
+<a 
+href="https://github.com/evatok/weather-react-app"
+ target="_blank" > open-sourced on GitHub</a>
+ </footer>
     </div>
   );
 }
