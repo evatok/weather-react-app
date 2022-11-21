3b97ad9135c4dabe030b4e656ef6e90eb4c664c1
evatok <teo@ukr.net>
2022-11-21 17:40:18 +0100

    added html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Weather from "./Weather"
 
 export default function App() {
   return (
     <div className="App container">
-<h1>Weather App</h1>
+
+<Weather />
 
 <footer>
   This project was coded by {" "}<a href="https://bucolic-liger-7233b0.netlify.app/index.html" target="_blank" rel="noreferrer" >Eva Tokarchuk</a>
