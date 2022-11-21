cf5ec1f4cfdc2aa9180077bd5d4103d8e111467a
evatok <teo@ukr.net>
2022-11-21 15:23:44 +0100

    added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ export default function App() {
 <h1>Weather App</h1>
 <footer>
   This project was coded by {" "}
-  <a href="https://bucolic-liger-7233b0.netlify.app/index.html" target="_blank">Eva Tokarchuk</a>
+  <a href="https://bucolic-liger-7233b0.netlify.app/index.html" target="_blank" rel="noreferrer" >Eva Tokarchuk</a>
    {" "}and is{" "} 
 <a 
 href="https://github.com/evatok/weather-react-app"
- target="_blank" > open-sourced on GitHub</a>
+ target="_blank" rel="noreferrer" > open-sourced on GitHub</a>
  </footer>
     </div>
   );
