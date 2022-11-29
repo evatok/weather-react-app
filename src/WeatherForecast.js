4e4a7942f166a370a7c415b0a5b21d1fe3265b72
evatok <teo@ukr.net>
2022-11-29 15:20:09 +0100

    forecast functions for different locations

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css"
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
 let [loaded, setLoaded] = useState(false);
 let [forecast, setForecast] = useState(null);
+
+useEffect(() => {
+  setLoaded(false);
+}, [props.coordinates]);
 
     function handleResponse(response) {
         setForecast(response.data.daily);
@@ -15,19 +19,25 @@ let [forecast, setForecast] = useState(null);
    
 
     if (loaded) {    
+        console.log(forecast)
         return (
             <div className="WeatherForecast">
               <div className="row mt-4">
-                {forecast.map(function(dailyForecast, index) {
-                    if (index < 5) {
-                    return (
-                        <div className="col" key={index}>
-                        <WeatherForecastDay data={dailyForecast} />
-                        </div>
-                    );
-                 }
-                })}
-               
+                <div className="col">
+                  <WeatherForecastDay data={forecast[0]} />
+                </div>
+                <div className="col">
+                  <WeatherForecastDay data={forecast[1]} />
+                </div> 
+                <div className="col">
+                  <WeatherForecastDay data={forecast[2]} />
+                </div> 
+                <div className="col">
+                  <WeatherForecastDay data={forecast[3]} />
+                </div> 
+                <div className="col">
+                  <WeatherForecastDay data={forecast[4]} />
+                </div>
               </div>  
             </div>
         
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,29 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("celsius");
-function showFahrenheit(event) {
-   event.preventDefault();
-   setUnit("fahrenheit") 
-}
-function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius") 
- }
 
-  if (unit === 'celsius') { 
 return (
     <span className="WeatherTemperature"> 
       <span className="temperature" >{Math.round(props.celsius)}</span>
-      <span className="units">°C | <a href="/" onClick={showFahrenheit}>°F</a></span>
+      <span className="units">°C</span>
     </span>
     );
-  } else {
-    let fahrenheit = (props.celsius * 9/5) + 32;
-    return  ( <span className="WeatherTemperature"> 
-    <span className="temperature" >{Math.round(fahrenheit)}</span>
-    <span className="units"><a href="/" onClick={showCelsius}>°C</a> | °F</span>
-  </span>
-  );
-  }
+
 }
