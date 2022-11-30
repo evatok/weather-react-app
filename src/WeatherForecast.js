e34f5cf5506189724e58a6e3b6e2150250d5604b
evatok <teo@ukr.net>
2022-11-30 13:45:54 +0100

    refactored code

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,6 +15,13 @@ useEffect(() => {
         setForecast(response.data.daily);
         setLoaded(true);
     }
+    function load() {
+      let apiKey = "b9ba0314a93083136d968577c718e31d";
+      let lat = props.coordinates.lat;
+      let lon = props.coordinates.lon;
+      let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+    }
 
     if (loaded) {
         return (
@@ -35,11 +42,8 @@ useEffect(() => {
             </div>
            );
     } else  {
-        let apiKey = "b9ba0314a93083136d968577c718e31d";
-        let lat = props.coordinates.lat;
-        let lon = props.coordinates.lon;
-        let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse);
+       load()
+       
         return null;
     }
 }
