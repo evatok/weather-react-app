e611f111c8787a3d63df7a927c293e2b5be304c6
evatok <teo@ukr.net>
2022-11-30 13:17:51 +0100

    5 days + map

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -13,9 +13,19 @@ let [forecast, setForecast] = useState(null);
         return (
             <div className="WeatherForecast">
               <div className="row mt-4">
-              <div className="col">
-              <WeatherForecastDay data={forecast[0]} />
-              </div>
+                {forecast.map(function(dailyForecast, index) {
+                  if (index < 5) {
+                    return (
+                      <div className="col" key={index}>
+                      <WeatherForecastDay data={dailyForecast} />
+                      </div>
+                    )
+                  } else {
+                    return null
+                  }
+               
+                })}
+            
               </div>
             </div>
           
@@ -23,8 +33,8 @@ let [forecast, setForecast] = useState(null);
     }
     else  {
         let apiKey = "b9ba0314a93083136d968577c718e31d";
-        let lat = props.coordinates.lon;
-        let lon = props.coordinates.lat;
+        let lat = props.coordinates.lat;
+        let lon = props.coordinates.lon;
         let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
         return null;
